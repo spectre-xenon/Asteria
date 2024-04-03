42664b8432e29569ceddcc6c9ed8513bea5e6ab2
Spectre-Xenon <62655598+spectre-xenon@users.noreply.github.com>
2024-04-03 22:23:21 +0200

    chore: added Inter font and default style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,9 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
