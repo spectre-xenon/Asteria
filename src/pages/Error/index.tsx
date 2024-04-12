4893050b070a4d8aa500e12bfabb938c7a82c2cb
Spectre-Xenon <62655598+spectre-xenon@users.noreply.github.com>
2024-04-12 14:49:27 +0200

    feat: finished Error page

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -1,10 +1,17 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 
 const Error: FC = () => {
   return (
-    <>
-      <h1>404</h1>
-    </>
+    <div className="flex h-screen flex-col items-center justify-center gap-7">
+      <h1 className="text-4xl  md:text-7xl">404: NOT FOUND</h1>
+      <Link
+        to="/"
+        className="rounded-full bg-white px-4 py-1 text-black transition-transform ease-in-out hover:scale-105"
+      >
+        Home ➜
+      </Link>
+    </div>
   );
 };
 
