d9e222080abc2d0ef9ce453cf2d5c29ed66fa8f4
Spectre-Xenon <62655598+spectre-xenon@users.noreply.github.com>
2024-04-12 19:20:39 +0200

    fix: restore remove React.StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "@/routes";
@@ -6,5 +7,7 @@ import "./index.css";
 const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
